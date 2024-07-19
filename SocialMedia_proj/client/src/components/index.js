efba37636880edeecc0de31583b74cbd23ba0a73
Sadhvireddybuddannagari <sadhvireddybuddannagari@gmail.com>
2024-07-19 19:32:16 +0530

    Update index.js

--- SocialMedia_proj/client/src/components/index.js
+++ SocialMedia_proj/client/src/components/index.js
@@ -1,4 +1,8 @@
 import TextInput from "./TextInput";
 import Loading from "./Loading";
 import CustomButton from "./CustomButton";
-export {TextInput,Loading,CustomButton};
+import TopBar from "./TopBar";
+import ProfileCard from "./ProfileCard";
+import FriendsCard from "./FriendsCard";
+
+export {TextInput,Loading,CustomButton,TopBar,ProfileCard,FriendsCard};
